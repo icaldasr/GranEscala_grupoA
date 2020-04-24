6a0e62d59a95f4c85e050e62f0eb7a4f91a99aa1
camilo hernandez <juaca2009@gmail.com>
2020-04-24 16:53:54 -0500

    inicio de la api

---
+++ API/api.py
@@ -0,0 +1,20 @@
+import pymysql
+from flask import Flask
+
+conexion = pymysql.Connect(host='mysql-historiasclinicas.alwaysdata.net', 
+                           user='203256', password='juancamilo99', db='historiasclinicas_bd')
+cursor = conexion.cursor()
+
+app = Flask(__name__)
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
