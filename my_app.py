2da63b5277eeb50a2a4e9e015613968ade2fb67a
Luis Miguel Oviedo L <mimi1296@javerianacali.edu.co>
2020-04-29 21:10:42 -0500

    Pagina incial administrado
    + vínculos a Buscar, registrar doctor, regitstrar paciente

--- my_app.py
+++ my_app.py
@@ -7,11 +7,6 @@ from clases.Sistema import Sistema
 
 app = Flask(__name__)
 app.secret_key = "ValleDev1234"
-#app.config['MYSQL_USER'] = '203256'
-#app.config['MYSQL_PASSWORD'] = 'juancamilo99'
-#app.config['MYSQL_HOST'] = 'mysql-historiasclinicas.alwaysdata.net'
-#app.config['MYSQL_DB'] = 'historiasclinicas_bd'
-#mysql = MySQL(app)
 sis = Sistema()
 
 @app.route("/", methods = ["POST", "GET"])
@@ -31,7 +26,7 @@ def login():
             return render_template("login.html")
     else:
         if "user" in session:
-            return redirect(url_for("logout"))
+            return redirect(url_for("logout"))  ##aqui se puede cambiar la conf para que cuando se cierre el navegador se mantenga la sesion iniciada
         return render_template("login.html")
 
 
@@ -39,21 +34,43 @@ def login():
 def admin():
     if "user" in session:
         usuario = session["user"]
-        return f"<h1>{usuario}</h1>"
+        return render_template("admin.html")
     else:
         return redirect(url_for("login"))
+
+@app.route("/Buscar", methods = ["POST", "GET"])
+def buscar():
+    return render_template("admin_2.html")
+
+@app.route("/registrarDoctor", methods = ["POST", "GET"])
+def registrarDoctor():
+    return "Registro Doctor"##redirect(url_for("registrarMedico.html"))  ##No existe aun
+
+
+@app.route("/registrarPaciente", methods = ["POST", "GET"])
+def registrarPaciente():
+    return "Registro Paciente"##redirect(url_for("registrarPaciente.html"))  ##No existe aun
+
+@app.route("/registrarAdministrador", methods = ["POST", "GET"])
+def registrarAdministrador():
+    return redirect(url_for("registrarAdministador.html"))  ##No existe aun
+
 
 
 @app.route("/doctor", methods = ["POST", "GET"])
 def doctor():
     if "user" in session:
         usuario = session["user"]
-        return f"<h1>{usuario}</h1>"
+        return f"<h1>{usuario}</h1>" ##Debe mostrar la página inicial de doctor
     else:
         return redirect(url_for("login"))
 
 
 @app.route("/logout")
 def logout():
+    if "user" in session:
+        usuario = session["user"]
+        #flash("Se cerró sesión correctamente", "info")
     session.pop("user", None)
+    
     return redirect(url_for("login"))
